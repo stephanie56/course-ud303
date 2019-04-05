1e6bae7e68a8ba631250aacd74d3c8b359918097
Stephanie Zeng <stephanie.zeng@rangle.io>
2019-04-04 20:51:34 -0400

    Complete bookmark server

--- Lesson-2/7_BookmarkServer/BookmarkServer.py
+++ Lesson-2/7_BookmarkServer/BookmarkServer.py
@@ -74,9 +74,13 @@ def CheckURI(uri, timeout=5):
     False if that GET request returns any other response, or doesn't return
     (i.e. times out).
     '''
-    # 1. Write this function.  Delete the following line.
-    raise NotImplementedError("Step 1 isn't written yet.")
-
+    # 1. Write this function.  Delete the following line
+    try:
+        r = requests.get(uri, timeout=timeout)
+        return r.status_code == 200
+    except requests.RequestException:
+        return False
+    
 
 class Shortener(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -88,7 +92,9 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             if name in memory:
                 # 2. Send a 303 redirect to the long URI in memory[name].
                 #    Delete the following line.
-                raise NotImplementedError("Step 2 isn't written yet.")
+                self.send_response(303)
+                self.send_header("Location", memory[name])
+                self.end_headers()
             else:
                 # We don't know that name! Send a 404 error.
                 self.send_response(404)
@@ -115,7 +121,11 @@ class Shortener(http.server.BaseHTTPRequestHandler):
         if "longuri" not in params or "shortname" not in params:
             # 3. Serve a 400 error with a useful message.
             #    Delete the following line.
-            raise NotImplementedError("Step 3 isn't written yet!")
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("Missing form fields!".encode())
+            return
 
         longuri = params["longuri"][0]
         shortname = params["shortname"][0]
@@ -126,13 +136,18 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
             # 4. Serve a redirect to the root page (the form).
             #    Delete the following line.
-            raise NotImplementedError("Step 4 isn't written yet!")
+            self.send_response(303)
+            self.send_header("Location", "/")
+            self.end_headers()
         else:
             # Didn't successfully fetch the long URI.
 
             # 5. Send a 404 error with a useful message.
             #    Delete the following line.
-            raise NotImplementedError("Step 5 isn't written yet!")
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write('Failed to find "{}"'.format(longuri).encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)
